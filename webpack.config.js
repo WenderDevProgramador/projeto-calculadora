ac57af5c62c513b160e947078dbcd1133b81d031
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2024-10-12 09:44:23 -0300

    atualização de desempenho

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const { default: test } = require("node:test");
+
+module.exports = {
+    entry: {
+        index: './src/script/index.js'
+    },
+    mode: 'production',
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        },{
+            test: /\.js$/,
+            use: ['babel-loader']
+        }]
+    },
+    output: {
+        filename: '[name].min.js'
+    }
+}
